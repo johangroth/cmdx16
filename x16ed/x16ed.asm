0295dd0320a5f5dea6d3f506c9f4947ad286acb8
Johan Groth <johangroth1@gmail.com>
2019-12-05 06:05:43 +0000

    Clear screen doesn't work.

--- x16ed/x16ed.asm
+++ x16ed/x16ed.asm
@@ -8,7 +8,8 @@
         .byte $0e, $08, $0a, $00, $9e, $20, $28, $32, $30, $36, $34, $29, $00, $00, $00
         * = $0810
 
-        stz 204
+        jsr set_mode
+        jsr clear_screen
 next:
         jsr getin
         jsr chrout
@@ -17,3 +18,56 @@ next:
 done:
         rts
 hello   .null "hello world"
+
+set_mode: .proc
+        lda #$0f            ;Bank 4
+        sta vera_inc
+        stz vera_hi
+        lda #$41            ;H-scale
+        sta vera_lo
+        ldy text_mode
+        lda modex,y
+        sta vera_rw
+        lda #$42            ;V-scale
+        sta vera_lo
+        lda modey,y
+        sta vera_rw
+        stz vera_inc        ;Bank 0
+        lda sizex,y
+        sta scr_size_x
+        lda sizey,y
+        sta scr_size_y
+        rts
+        .pend
+
+clear_screen: .proc
+        lda #%00010000      ;Set VERA increment to 1
+        sta vera_inc
+        stz vera_hi
+        stz vera_lo
+        ldy #60
+clsc01:
+        inc vera_hi
+        ldx #80
+clsc02:
+        lda #32
+        sta vera_rw
+        lda #1
+        sta vera_rw
+        dex
+        bne clsc02
+        dey
+        bne clsc01
+        rts
+        .pend
+
+modex:  .byte 128,128,64,64,32,32
+modey:  .byte 128,64,128,64,64,32
+sizex:  .byte 80,80,40,40,20,20
+sizey:  .byte 60,30,60,30,30,15
+text_mode:
+        .byte 0             ;Default 0=80x60
+scr_size_x:
+        .byte   80          ;Screen size X (20, 40, or 80)
+scr_size_y;
+        .byte   60          ;Screen size Y (15, 30, or 60)
